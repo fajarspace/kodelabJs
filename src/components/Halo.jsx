01e3dc248940f419e5f3d487e3e736c91f745b95
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-11-19 19:39:37 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Sat Nov 19 19:39:37 WIB 2022

---
+++ src/components/Halo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Halo = () => {
+  return (
+    <div>
+      <h1 className='title' >haloo</h1>
+      <h2 className='subtitle'> welkom bek . . . </h2>
+    </div>
+  )
+}
+
+export default Halo
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from "./Layout";
+
+const Dashboard = () => {
+  return (
+    <Layout>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores nobis dolorum ipsam laborum sit animi! Explicabo ipsa officiis id mollitia velit quis odit corporis, earum sunt? Quos officiis perferendis laborum.
+    </Layout>
+  )
+}
+
+export default Dashboard
