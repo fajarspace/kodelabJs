e93602105384539627f305caeedb595756a22e94
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-11-19 22:27:43 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Sat Nov 19 22:27:43 WIB 2022

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className='container' >
         <nav>
           <ul>
-            <li className="title">dasd</li>
+            <li className="title">Kodelab</li>
             <li>
-              asd
+            <NavLink to={"/dashboard"} >
+              Login
+            </NavLink>
             </li>
           </ul>
         </nav>
-        </header>
+    </header>
   )
 }
 
